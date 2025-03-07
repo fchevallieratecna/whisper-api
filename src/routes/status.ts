d17923674d8db27f3066d5e87894ccabf85d7833
Florian Chevallier <fchevallier@atecna.fr>
2025-03-07 15:37:50 +0100

    feat: implement application status and version endpoint
    - Add a new route for fetching the status of the application
    - Include functionality to retrieve the version of `whisperx_cli`
    - Determine the operating system and recommended compute type based on the platform
    - Return a JSON response with the status, whisperx_cli version, OS name, and recommended compute type
    
    Signed-off-by: Florian Chevallier <fchevallier@atecna.fr>

---
+++ src/routes/status.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response } from 'express';
+import os from 'os';
+import { execSync } from 'child_process';
+
+const router = Router();
+
+router.get('/status', (req: Request, res: Response) => {
+  let whisperVersion;
+  try {
+    whisperVersion = execSync('whisperx_cli --version', { encoding: 'utf8' }).trim();
+  } catch (error) {
+    whisperVersion = 'whisperx_cli introuvable ou ne fonctionne pas';
+  }
+
+  const platform = os.platform();
+  let osName: string, computeType: string;
+  switch(platform) {
+    case 'win32':
+      osName = 'windows';
+      computeType = 'int8';
+      break;
+    case 'darwin':
+      osName = 'mac';
+      computeType = 'float16';
+      break;
+    case 'linux':
+      osName = 'linux';
+      computeType = 'float32';
+      break;
+    default:
+      osName = platform;
+      computeType = 'float16';
+  }
+
+  res.status(200).json({
+    status: 'ok',
+    whisperx_cli_version: whisperVersion,
+    os: osName,
+    recommended_compute: computeType
+  });
+});
+
+export default router; 
